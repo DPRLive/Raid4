2b446df93eae69495eca8a0ace22bf6b7b100986
Imusun <imusun190812@gmail.com>
2023-12-29 03:17:25 +0900

    Chore: R4GameInstance 추가 및 GameInstance 설정 변경

---
+++ Source/Raid4/Core/R4GameInstance.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "R4GameInstance.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(R4GameInstance)
---
+++ Source/Raid4/Core/R4GameInstance.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include <Engine/GameInstance.h>
+#include "R4GameInstance.generated.h"
+
+/**
+ * GameInstance
+ */
+UCLASS()
+class RAID4_API UR4GameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
