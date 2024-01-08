6fdc4bbea09fa64159632fdeade8642161c3e1d3
Imusun <imusun190812@gmail.com>
2024-01-09 00:36:12 +0900

    Feat(Input): 캐릭터 이동 입력 추가
    - short click / long click을 감지하여 이동
    - 목표로 이동시 SimpleMoveToLocation 로 Location 까지 이동하며, Character Movement Comp의 Replicate를 사용하기 위해 ( 요청된 이동 가속 사용 , 경로에 가속 사용 ) on
    - 프로젝트 세팅에서 클라이언트 측면 내비게이션 허용 on

--- Source/Raid4/Component/R4PlayerInputComponent.cpp
+++ Source/Raid4/Component/R4PlayerInputComponent.cpp
@@ -4,12 +4,22 @@
 #include "R4PlayerInputComponent.h"
 #include "../Character/PlayerCharacter.h"
 
+#include <EnhancedInputComponent.h>
+#include <EnhancedInputSubsystems.h>
+#include <GameFramework/PlayerController.h>
+#include <Engine/LocalPlayer.h>
+#include <Blueprint/AIBlueprintHelperLibrary.h>
+#include <GameFramework/CharacterMovementComponent.h>
+
 #include UE_INLINE_GENERATED_CPP_BY_NAME(R4PlayerInputComponent)
 
 UR4PlayerInputComponent::UR4PlayerInputComponent()
 {
 	PrimaryComponentTick.bCanEverTick = false;
 	bWantsInitializeComponent = true;
+	
+	ShortTriggerThreshold = 0.3f;
+	TriggerTime = 0.f;
 }
 
 /**
@@ -37,7 +47,120 @@ void UR4PlayerInputComponent::BeginPlay()
 /**
  *  입력을 바인딩 하는 함수
  */
-void UR4PlayerInputComponent::_InitializePlayerInput(UInputComponent* PlayerInputComponent)
+void UR4PlayerInputComponent::_InitializePlayerInput(UInputComponent* InPlayerInputComponent)
 {
+	// 액션 바인딩
+	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InPlayerInputComponent))
+	{
+		EnhancedInputComponent->ClearActionBindings();
+
+		// 이동 입력 액션 바인딩
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Started, this, &UR4PlayerInputComponent::OnInputMoveStarted);
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &UR4PlayerInputComponent::OnInputMoveTriggered);
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Completed, this, &UR4PlayerInputComponent::OnInputMoveCompleted);
+	}
+
+	APawn* owner = Cast<APawn>(GetOwner());
+	if(owner == nullptr)
+	{
+		LOG_WARN(R4Input, TEXT("Owner is nullptr."));
+		return;
+	}
+
+	APlayerController* playerController = Cast<APlayerController>(owner->GetController());
+	if(playerController == nullptr)
+	{
+		LOG_WARN(R4Input, TEXT("Owner's Controller is nullptr."));
+		return;
+	}
 	
+	// IMC 바인딩
+	if(UEnhancedInputLocalPlayerSubsystem* subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(playerController->GetLocalPlayer()))
+	{
+		if(subsystem->HasMappingContext(InputMapping))
+		{
+			LOG_WARN(R4Input, TEXT("Owner already has this mapping context."));
+			return;
+		}
+
+		subsystem->AddMappingContext(InputMapping, 0);
+	}
+}
+
+/**
+ *  이동 입력 시작
+ */
+void UR4PlayerInputComponent::OnInputMoveStarted()
+{
+	if(ACharacter* owner = Cast<ACharacter>(GetOwner()))
+	{
+		owner->GetMovementComponent()->StopMovementImmediately();
+	}
+}
+
+/**
+ *  이동 입력 Triggered
+ */
+void UR4PlayerInputComponent::OnInputMoveTriggered()
+{
+	APawn* owner = Cast<APawn>(GetOwner());
+	if(owner == nullptr)
+	{
+		LOG_WARN(R4Input, TEXT("Owner is nullptr."));
+		return;
+	}
+	
+	APlayerController* playerController = Cast<APlayerController>(owner->GetController());
+	if(playerController == nullptr)
+	{
+		LOG_WARN(R4Input, TEXT("Owner's Controller is nullptr."));
+		return;
+	}
+
+	TriggerTime += GetWorld()->GetDeltaSeconds();
+	
+	FHitResult hit;
+	if(playerController->GetHitResultUnderCursor(ECollisionChannel::ECC_Visibility, false, hit))
+	{
+		FVector dir = hit.Location - owner->GetActorLocation();
+		dir.Z = 0.f;
+		dir.Normalize();
+		
+		owner->AddMovementInput(dir);
+		LastHitLocation = hit.Location;
+	}
+}
+
+/**
+ *  이동 입력 종료
+ */
+void UR4PlayerInputComponent::OnInputMoveCompleted()
+{
+	ACharacter* owner = Cast<ACharacter>(GetOwner());
+	if(owner == nullptr)
+	{
+		LOG_WARN(R4Input, TEXT("Owner is nullptr."));
+		return;
+	}
+
+	APlayerController* playerController = Cast<APlayerController>(owner->GetController());
+	if(playerController == nullptr)
+	{
+		LOG_WARN(R4Input, TEXT("Owner's Controller is nullptr."));
+		return;
+	}
+
+	// 짧은 입력이면, 위치로 이동 실행
+	if(TriggerTime < ShortTriggerThreshold)
+	{
+		UAIBlueprintHelperLibrary::SimpleMoveToLocation(playerController, LastHitLocation);
+		UtilEffect::SpawnNiagaraAtLocation_Local(FXCursor, LastHitLocation, FRotator::ZeroRotator, FVector(1.f), GetWorld());
+	}
+	else
+	{
+		// 긴 입력이었다면, 멈춤
+		owner->GetMovementComponent()->StopMovementImmediately();
+	}
+	
+	TriggerTime = 0.f;
 }
--- Source/Raid4/Component/R4PlayerInputComponent.h
+++ Source/Raid4/Component/R4PlayerInputComponent.h
@@ -5,6 +5,10 @@
 #include <Components/ActorComponent.h>
 #include "R4PlayerInputComponent.generated.h"
 
+class UInputMappingContext;
+class UInputAction;
+class UNiagaraSystem;
+
 /**
  *  입력을 담당하는 Component
  */
@@ -18,11 +22,45 @@ public:
 
 	// 컴포넌트 초기화
 	virtual void InitializeComponent() override;
-	
+
 protected:
 	virtual void BeginPlay() override;
 
 private:
 	// 입력을 바인딩 하는 함수
-	void _InitializePlayerInput(UInputComponent* PlayerInputComponent);
+	void _InitializePlayerInput(UInputComponent* InPlayerInputComponent);
+	
+	// Move Input Action
+	void OnInputMoveStarted();
+	void OnInputMoveTriggered();
+	void OnInputMoveCompleted();
+	
+	// MappingContext
+	UPROPERTY( EditAnywhere, Category = "Data|Mapping", meta = (AllowPrivateAccess = true) )
+	TObjectPtr<UInputMappingContext> InputMapping;
+	
+	// 이동 입력 액션
+	UPROPERTY( EditAnywhere, Category = "Data|Action", meta = (AllowPrivateAccess = true) )
+	TObjectPtr<UInputAction> MoveAction;
+
+	// 짧은 입력 / 긴 입력을 구분하는 경계시간
+	UPROPERTY( EditAnywhere, Category = "Data|Setting", meta = (AllowPrivateAccess = true) )
+	float ShortTriggerThreshold;
+
+	// 클릭시 스폰되는 FX (어디를 클릭했는지 표시)
+	UPROPERTY( EditAnywhere, Category = "Data|Setting", meta=(AllowPrivateAccess = true) )
+	TSoftObjectPtr<UNiagaraSystem> FXCursor;
+
+	/////////////////////////
+	// **** Transient **** //
+	/////////////////////////
+	
+	// 눌린 시간을 측정하는 임시 변수
+	UPROPERTY( Transient )
+	float TriggerTime;
+
+	// 마지막에 눌린 위치를 기억하는 변수
+	UPROPERTY( Transient )
+	FVector LastHitLocation;
+
 };
