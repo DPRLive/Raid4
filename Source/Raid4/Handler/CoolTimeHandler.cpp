b217e8c80ccceeeb246bab523697abae888969bd
Imusun <imusun190812@gmail.com>
2024-02-05 01:49:24 +0900

    Feat: CoolTime을 쉽게 걸게 하는 Handler객체 추가
    - 내부적으로는 UE Timer를 사용

---
+++ Source/Raid4/Handler/CoolTimeHandler.cpp
@@ -0,0 +1,77 @@
+﻿#include "CoolTimeHandler.h"
+
+#include <TimerManager.h>
+
+/**
+ * 생성자
+ */
+FCoolTimeHandler::FCoolTimeHandler()
+{
+   Handle = FTimerHandle();
+}
+
+/**
+ * 소멸자
+ */
+FCoolTimeHandler::~FCoolTimeHandler()
+{
+    // 람다 내부에서 this를 캡처해갔으므로 위험할 수 있으니 소멸 시 clear.
+    ClearCoolTime();
+}
+
+/**
+ * 쿨타임을 적용한다.
+ * @param InCoolTime : 사용할 쿨타임. 0.f >= InCoolTime이면 clear됨
+ */
+void FCoolTimeHandler::SetCoolTime(const float InCoolTime)
+{
+	R4GetWorld()->GetTimerManager().SetTimer(Handle, [this]
+	{
+	    if(OnCompletedCoolTime.IsBound())
+	        OnCompletedCoolTime.Broadcast();
+	}, InCoolTime, false);
+}
+
+/**
+ * 쿨타임을 해제한다.
+ */
+void FCoolTimeHandler::ClearCoolTime()
+{
+    R4GetWorld()->GetTimerManager().ClearTimer(Handle);
+}
+
+/**
+ * 남은 쿨타임을 가져온다.
+ * @return : 쿨타임을 반환, 쿨타임이 걸려있지 않은경우 0.f를 반환
+ */
+const float FCoolTimeHandler::GetCoolTime() const
+{
+    if(R4GetWorld()->GetTimerManager().IsTimerActive(Handle))
+        return R4GetWorld()->GetTimerManager().GetTimerRemaining(Handle);
+
+    return 0.f;
+}
+
+/**
+ * 쿨타임을 감소시킨다. 감소 후 쿨타임이 0보다 작거나 같으면 완료된걸로 간주.
+ * @param InReduceTime : 감소시키고 싶은 시간
+ * @return : 감소된 후 쿨타임. 남은 쿨타임보다 감소량이 더 많으면 0.f 반환 
+ */
+float FCoolTimeHandler::ReduceCoolTime(const float InReduceTime)
+{
+    float newCoolTime = FMath::Clamp(GetCoolTime() - InReduceTime, 0.f, GetCoolTime() - InReduceTime);
+
+	// 감소했는데 0이면 완료된거로 간주
+	if(FMath::IsNearlyZero(newCoolTime))
+	{
+		ClearCoolTime();
+
+		if(OnCompletedCoolTime.IsBound())
+			OnCompletedCoolTime.Broadcast();
+		
+		return newCoolTime;
+	}
+
+	SetCoolTime(newCoolTime);
+	return newCoolTime;
+}
---
+++ Source/Raid4/Handler/CoolTimeHandler.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+
+/**
+ * 객체가 쿨타임을 걸 수 있게 하는 클래스
+ * BeginDestroy시 Clear하면 좋을듯
+ */
+class FCoolTimeHandler
+{
+public:
+	// 생성자
+	FCoolTimeHandler();
+
+	// 소멸자
+	virtual ~FCoolTimeHandler();
+	
+	// 쿨타임을 적용한다.. 중복으로 쿨타임을 걸 수는 없음
+	void SetCoolTime(const float InCoolTime);
+
+	// 쿨타임을 해제한다.
+	void ClearCoolTime();
+
+	// 남은 쿨타임을 가져온다.
+	const float GetCoolTime() const;
+
+	// 쿨타임을 감소시킨다. 감소 후 쿨타임이 0보다 작거나 같으면 완료된걸로 간주.
+	float ReduceCoolTime(const float InReduceTime);
+	
+	// 쿨타임이 완료되면 broadcast할 delegate
+	DECLARE_MULTICAST_DELEGATE( FCompletedCoolTime )
+	FCompletedCoolTime OnCompletedCoolTime;
+	
+private:
+	// Timer를 사용하기 위한 Handle
+    FTimerHandle Handle;
+};
