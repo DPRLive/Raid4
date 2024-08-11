2969d9799f1d9763f305c5552d627e954d480f56
Imusun <imusun190812@gmail.com>
2024-08-12 01:04:30 +0900

    Feat: Buff Damage Applier 추가.
    - Damage를 받도록 하는 Buff Class
    - 데미지 특성 상 Deactivate 불가.
    - BuffDesc의 Value가 DamageApplyDesc의 Value로 세팅되고, DamageApplyDesc의 Value는 사용되지 않음!

---
+++ Source/Raid4/Buff/Damage/R4Buff_DamageApplier.cpp
@@ -0,0 +1,53 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "R4Buff_DamageApplier.h"
+#include "../../Damage/R4DamageReceiveInterface.h"
+#include "../../Util/UtilDamage.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(R4Buff_DamageApplier)
+
+UR4Buff_DamageApplier::UR4Buff_DamageApplier()
+{
+	DamageApplyDesc = FR4DamageApplyDesc();
+}
+
+/**
+ *  버프 적용 전 세팅
+ *  @param InInstigator : 버프를 시전한 액터
+ *  @param InVictim : 버프를 적용할 대상
+ *  @param InBuffDesc : 버프 적용 시 기본 클래스에서 설정한 값 말고 다른 값이 필요한 경우 적용.
+ *  BuffDesc.Value : FR4DamageApplyDesc의 Value 값.
+ */
+bool UR4Buff_DamageApplier::PreActivate(AActor* InInstigator, AActor* InVictim, const FR4BuffDesc* InBuffDesc)
+{
+	bool bReady = Super::PreActivate(InInstigator, InVictim, InBuffDesc);
+
+	// BuffDesc의 Value로 DamageApplyDesc의 Value를 설정
+	DamageApplyDesc.Value = BuffDesc.Value;
+	
+	if(!InVictim->GetClass())
+		return false;
+	
+	// 상대가 데미지를 받을 수 있는 객체인지 확인
+	return bReady && InVictim->GetClass()->ImplementsInterface(UR4DamageReceiveInterface::StaticClass());
+}
+
+void UR4Buff_DamageApplier::Activate()
+{
+	Super::Activate();
+
+	// 데미지 인터페이스를 통해 데미지 전달
+	if(IR4DamageReceiveInterface* victim = Cast<IR4DamageReceiveInterface>(CachedVictim))
+	{
+		// 데미지 계산
+		FR4DamageReceiveInfo damageInfo = UtilDamage::CalculateDamageReceiveInfo(CachedInstigator.Get(), CachedVictim.Get(), DamageApplyDesc);
+		victim->ReceiveDamage(CachedInstigator.Get(), damageInfo);
+	}
+}
+
+void UR4Buff_DamageApplier::Clear()
+{
+	Super::Clear();
+	// Buff Damage는 어차피 BuffDesc의 Value로 데미지가 결정 되므로
+	// private한 Damage Desc를 되돌릴 필요가 아직은 없음
+}
---
+++ Source/Raid4/Buff/Damage/R4Buff_DamageApplier.h
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "../R4BuffBase.h"
+#include "../../Damage/R4DamageStruct.h"
+#include "R4Buff_DamageApplier.generated.h"
+
+/**
+ * Damage를 입히는 Buff.
+ * Damage Interface를 거쳐서 데미지를 전달.
+ * Deactivate 불가.
+ */
+UCLASS( HideDropdown, NotBlueprintType, Blueprintable, ClassGroup=(Buff), HideCategories=(Deactivate) )
+class RAID4_API UR4Buff_DamageApplier : public UR4BuffBase
+{
+	GENERATED_BODY()
+
+public:
+	UR4Buff_DamageApplier();
+
+protected:
+	// 버프가 적용 전 해야 할 로직 (세팅 등)해야 하는 것을 정의.
+	virtual bool PreActivate(AActor* InInstigator, AActor* InVictim, const FR4BuffDesc* InBuffDesc = nullptr) override;
+	
+	// 버프가 실제로 할 로직을 정의
+	virtual void Activate() override;
+
+	// 버프를 Clear하는 로직을 정의
+	virtual void Clear() override;
+
+private:
+	// 가할 데미지 관련 정보를 설정. BuffDesc의 Value가 DamageApplyDesc의 Value로 세팅.
+	// Buff에서 DamageApplyDesc의 Value는 사용되지 않음!
+	UPROPERTY( EditDefaultsOnly, meta = (AllowPrivateAccess = true))
+	FR4DamageApplyDesc DamageApplyDesc;
+};
