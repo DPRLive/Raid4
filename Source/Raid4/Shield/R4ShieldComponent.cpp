18d3f08d74c5631cd9db1ec7eff51bf083a2a118
Imusun <imusun190812@gmail.com>
2024-08-19 23:14:51 +0900

    Feat: 방어막 기능을 부여해주는 R4ShieldComponent 추가
    - UObject*를 키로 중복이 안되도록 관리
    - 총 방어막량을 Replicate
    - 삽입, 삭제(대게 앞에서 일어남), 순회가 필요해서 DoubleLinkedList 사용
    - 아직 테스트x, UI 만들고 난 뒤 테스트 예정

---
+++ Source/Raid4/Shield/R4ShieldComponent.cpp
@@ -0,0 +1,137 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "R4ShieldComponent.h"
+
+#include <Net/UnrealNetwork.h>
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(R4ShieldComponent)
+
+UR4ShieldComponent::UR4ShieldComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+
+	SetIsReplicatedByDefault(true);
+	TotalShield = 0.f;
+}
+
+/**
+ *	GetLifetimeReplicatedProps
+ */
+void UR4ShieldComponent::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(UR4ShieldComponent, TotalShield);
+}
+
+/**
+ *	InValue 만큼의 Shield를 추가. InProvideObj에 따라서 방어막을 부여한 객체를 구분, 동일 객체로부터 여러개의 방어막 중첩 불가
+ *	@param InProvideObj : 방어막을 부여할 객체, 동일 객체로부터 여러개의 방어막 중첩 불가
+ *	@param InValue : 부여할 방어막 값
+ */
+void UR4ShieldComponent::AddShield(UObject* InProvideObj, float InValue)
+{
+	// 중복 추가 방지, 찾았다면 제거
+	if(ShieldListNode* shield = _FindShieldByObject(InProvideObj))
+	{
+		TotalShield -= shield->GetValue().Value;
+		Shields.RemoveNode(shield);
+	}
+
+	// 방어막 추가
+	Shields.AddTail({InProvideObj, InValue});
+	TotalShield += InValue;
+
+	_OnRep_TotalShield();
+}
+
+/**
+ *	InValue 만큼의 Shield를 소모.
+ *	@param InValue : 소모할 방어막 값
+ *	@return : 총 소모한 방어막 값
+ */
+float UR4ShieldComponent::ConsumeShield(float InValue)
+{
+	float totalConsume = 0.f;
+
+	ShieldListNode* shield = Shields.GetHead();
+	while ( shield != nullptr || !FMath::IsNearlyZero(InValue) )
+	{
+		auto& [nowObj, nowShield] = shield->GetValue();
+		
+		// 소모해야 하는 양이 더 많거나 같은 경우
+		if(InValue > nowShield || FMath::IsNearlyEqual(InValue, nowShield))
+		{
+			InValue -= nowShield;
+			totalConsume += nowShield;
+			
+			Shields.RemoveNode(shield);
+			shield = Shields.GetHead();
+			continue;
+		}
+
+		// 아닌 경우 단순 소모
+		nowShield -= InValue;
+		totalConsume += InValue;
+		
+		InValue = 0.f;
+	}
+
+	// 방어막 소모 발생시
+	if(!FMath::IsNearlyZero(totalConsume))
+	{
+		TotalShield -= totalConsume;
+		_OnRep_TotalShield();
+	}
+	
+	return totalConsume;
+}
+
+/**
+ *	InProvideObj가 부여한 Shield를 제거
+ *	@param InProvideObj : 제거할 방어막을 부여한 객체
+ *	@return : 성공 여부
+ */
+bool UR4ShieldComponent::RemoveShield(UObject* InProvideObj)
+{
+	// 찾았다면 제거
+	if(ShieldListNode* shield = _FindShieldByObject(InProvideObj))
+	{
+		TotalShield -= shield->GetValue().Value;
+		Shields.RemoveNode(shield);
+
+		_OnRep_TotalShield();
+
+		return true;
+	}
+
+	return false;
+}
+
+/**
+ *  Shield Node 찾기
+ */
+UR4ShieldComponent::ShieldListNode* UR4ShieldComponent::_FindShieldByObject(UObject* InProvideObj) const
+{
+	ShieldListNode* node = Shields.GetHead();
+	
+	while ( node != nullptr )
+	{
+		if ( node->GetValue().Key == InProvideObj )
+			break;
+
+		node = node->GetNextNode();
+	}
+
+	return node;
+}
+
+/**
+ *  Total Shield가 변경되면, Broadcast
+ */
+void UR4ShieldComponent::_OnRep_TotalShield()
+{
+	if(OnChangeTotalShieldDelegate.IsBound())
+		OnChangeTotalShieldDelegate.Broadcast(TotalShield);
+}
---
+++ Source/Raid4/Shield/R4ShieldComponent.h
@@ -0,0 +1,59 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include <Components/ActorComponent.h>
+
+#include "R4ShieldComponent.generated.h"
+
+/**
+ * 방어막 기능을 부여해주는 컴포넌트.
+ * TODO : Test, UI Bind
+ */
+UCLASS(ClassGroup=(Shield), meta=(BlueprintSpawnableComponent))
+class RAID4_API UR4ShieldComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UR4ShieldComponent();
+
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+	
+public:
+	// InValue 만큼의 Shield를 추가. InProvideObj에 따라서 방어막을 부여한 객체를 구분, 동일 객체로부터 여러개의 방어막 중첩 불가
+	void AddShield(UObject* InProvideObj, float InValue);
+
+	// InValue 만큼의 Shield를 소모.
+	float ConsumeShield(float InValue);
+	
+	// InProvideObj가 부여한 Shield를 제거
+	bool RemoveShield(UObject* InProvideObj);
+
+	// 현재 총 방어막 수치를 return
+	FORCEINLINE float GetTotalShield() const { return TotalShield; }
+
+private:
+	// Shields 의 Node 타입
+	using ShieldListNode = TDoubleLinkedList<TTuple<TWeakObjectPtr<>, float>>::TDoubleLinkedListNode;
+	
+	// Shield Node 찾기
+	ShieldListNode* _FindShieldByObject(UObject* InProvideObj) const;
+	
+	// Total Shield가 변경되면, Broadcast
+	UFUNCTION()
+	void _OnRep_TotalShield();
+
+public:
+	// 방어막 총 수치 변경시 broadcast.
+	DECLARE_MULTICAST_DELEGATE_OneParam(FOnChangeTotalShieldDelegate, float);
+	FOnChangeTotalShieldDelegate OnChangeTotalShieldDelegate;
+	
+private:
+	// 현재 적용된 방어막
+	TDoubleLinkedList<TPair<TWeakObjectPtr<UObject>, float>> Shields;
+
+	// 현재 방어막의 총량
+	UPROPERTY(Transient, VisibleInstanceOnly, ReplicatedUsing = _OnRep_TotalShield )
+	float TotalShield;
+};
