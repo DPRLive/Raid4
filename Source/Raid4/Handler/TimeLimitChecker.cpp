81f9ae097f2b2cc5aaaed60953099062480404f3
Imusun <imusun190812@gmail.com>
2024-09-01 14:34:10 +0900

    Feat: Time Limit Checker 기능 추가
    - 서버시간을 기반으로 시간 제한을 '체크'만 하는 클래스.
    - Tick안씀, 대신 만료되었음을 알리는 Delegate가 없다.

---
+++ Source/Raid4/Handler/TimeLimitChecker.cpp
@@ -0,0 +1,44 @@
+﻿#include "TimeLimitChecker.h"
+
+FTimeLimitChecker::FTimeLimitChecker()
+{
+	TimeLimitDuration = 0.f;
+	TimeLimitStartServerTime = 0.f;
+}
+
+/**
+ *  새로운 Time 제한을 적용.
+ *  @param InTimeLimitDuration : 제한 시간
+ *  @param InServerTime : 제한시간 체크의 기준이 되는 시작시간(서버).
+ */
+void FTimeLimitChecker::SetNewTimeCheck( float InTimeLimitDuration, float InServerTime )
+{
+	TimeLimitDuration = InTimeLimitDuration;
+	TimeLimitStartServerTime = (InServerTime < 0.f ? R4GetServerTimeSeconds() : InServerTime);
+}
+
+/**
+ *  남은 제한 시간을 리턴
+ *  @param InServerTime : 현재 서버 시간.
+ */
+float FTimeLimitChecker::GetRemainingTime( float InServerTime ) const
+{
+	// 서버시간 안줬으면 R4GetServerTimeSeconds 사용
+	InServerTime = (InServerTime < 0.f ? R4GetServerTimeSeconds() : InServerTime);
+	float remainTime = TimeLimitStartServerTime + TimeLimitDuration - InServerTime;
+	
+	return FMath::Clamp(remainTime, 0.f, remainTime);
+}
+
+/**
+ *  제한시간이 지났는지 리턴
+ *  @param InServerTime : 현재 서버 시간.
+ */
+bool FTimeLimitChecker::IsTimeLimitExpired( float InServerTime ) const
+{
+	// 서버시간 안줬으면 R4GetServerTimeSeconds 사용
+	InServerTime = (InServerTime < 0.f ? R4GetServerTimeSeconds() : InServerTime);
+
+	// 남은 제한 시간이 0인지 확인
+	return FMath::IsNearlyZero(GetRemainingTime(InServerTime));
+}
---
+++ Source/Raid4/Handler/TimeLimitChecker.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+
+/**
+ * 특정 시간을 기록 후
+ * 현재 시간과 비교할 때 사용하기 좋은 FTimeLimitChecker.
+ * Tick을 굳이 사용해야 할 필요가 없을 때 사용하기 아주 좋지만
+ * Tick을 사용하지 않기에 delegate가 없음
+ */
+class FTimeLimitChecker
+{
+public:
+	FTimeLimitChecker();
+	
+	// 제한 시간을 리턴
+	FORCEINLINE float GetTimeLimit() const { return TimeLimitDuration; }
+	
+	// 제한 시간 설정
+	FORCEINLINE void SetTimeLimit( float InTimeLimitDuration ) { TimeLimitDuration = InTimeLimitDuration; }
+	
+	// 새로운 Time 제한 적용.
+	void SetNewTimeCheck( float InTimeLimitDuration, float InServerTime = -1.f );
+
+	// 남은 제한 시간을 리턴
+	float GetRemainingTime( float InServerTime = -1.f ) const;
+
+	// 제한시간이 지났는지 리턴
+	bool IsTimeLimitExpired( float InServerTime = -1.f ) const;
+	
+private:
+	// 제한 시간
+	float TimeLimitDuration;
+	
+	// 제한시간 체크의 기준이 되는 시작시간(서버).
+    float TimeLimitStartServerTime;
+};
