7b81cea73fb2344e6a61927f0d63c7db2e6c275c
Imusun <imusun190812@gmail.com>
2024-09-04 23:43:12 +0900

    Feat : Detect 시점을 알릴 수 있는 객체를 위한 interface, 해당 interface를 상속한 AnimNotify 추가
    - AnimNotify_NotifyDetect를 통해 특정 애니메이션 타이밍에 탐지 시점 알림을 받을 수 있음.
    - 단 Notify는 각 캐릭터마다 한개씩 들고 있는게 아니라 동일 캐릭터가 여러개 존재하는 경우 한 캐릭터 Anim을 실행했을 때 모두가 알림을 받아버리는 불상사가 발생할 수 있으므로 Actor*로 구분.

---
+++ Source/Raid4/Animation/Notify/AnimNotify_NotifyDetect.h
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "../../Detect/R4NotifyDetectInterface.h"
+
+#include "AnimNotify_NotifyDetect.generated.h"
+
+/**
+ * Detect할 타이밍을 알리는 Anim Notify
+ */
+UCLASS()
+class RAID4_API UAnimNotify_NotifyDetect : public UAnimNotify, public IR4NotifyDetectInterface
+{
+	GENERATED_BODY()
+
+public:
+	UAnimNotify_NotifyDetect();
+
+	virtual void Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference) override;
+
+public:
+	// ~ Begin IR4NotifyDetectInterface
+	virtual FSimpleDelegate& OnNotifyDetect( AActor* InReqActor ) override;
+	// ~ End IR4NotifyDetectInterface
+
+private:
+	// 탐지 시점을 알려주기 위한 탐지 요청 {객체, delegate} 캐싱
+	TArray<TPair<TWeakObjectPtr<AActor>, FSimpleDelegate>> CachedDetectable;
+};
