ce7b468567e8b9042257fb1f4f947c5444d8a991
Imusun <imusun190812@gmail.com>
2024-09-13 13:36:13 +0900

    Feat: Skill 구조 구체화 ( SkillBase, SkillAnimBase 분리 등)
    SkillBase
    - Skill Base에서는 Detect 관련 기능을 제공, Anim 관련 기능 제거
    - TimeLimitChecker는 Replicate가 안돼서 제거 후 Replicate할 수 있도록 스킬 쿨타임 체크 float 멤버 추가
    - Apply Buff, Apply Damage는 VIctim *를 넣어서 작동하도록 변경 및 Protected 설정. 근데 변경될 수도 있음
    - 기존 Detect 관리를 UObject* -> uint32 key값 BeginPlay 시 FR4SkillDetectInfo멤버 property parse를 통한 부여 방식으로 변경, 기존 DetectNoti* 로 체크시의 중복 문제 방지
    - FR4DetectInfo에 Key값 추가 및 각 함수에서 Key값 활용 (더미 제거 판별 시에도 해당 key값 이용)
    - Execute Detect도 protected 설정
    
    SkillAnimBase
    - 기존 SkillBase에 있던 Anim 관련 기능을 분리 및 SkillBase 상속을 통해 Anim을 사용하는 Skill을 위한 클래스 SkillAnimBase추가
    - FR4SkillAnimInfo도 uint32 Key값을 통해 구분하도록 구조 설정, DetectInfo와 유사하게 멤버 Property Parsing을 통해 서버에서 부여
    - 멤버 Property Parsing 시 DetectNoti <-> ExecuteDetect도 바인드
    - PlaySkillAnim으로 Anim Play시, OnBeginSkillAnim을 OwnerClient와 Server에서 같이 호출.
    - OnBeginSkillAnim()을 오버라이드 하여 키 값에 맞는 애님 호출 시 특정 로직을 호출할 수 있음 (Validation 체크에 유용하다)
    - IsLockPlaySkillAnim()을 통해서 ServerSide Anim Play Validation 체크 시 사용됨, 오버라이드 하여 체크 로직을 작성

---
+++ Source/Raid4/Skill/R4AnimSkillBase.h
@@ -0,0 +1,66 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "R4SkillBase.h"
+#include "R4AnimSkillBase.generated.h"
+
+class IR4NotifyDetectInterface;
+
+/**
+ * Skill Anim을 사용하는 Skill의 Base가 되는 클래스.
+ * 스킬을 위한 기능들을 제공
+ * < 상속하여 클래스를 제작 시 주의할 점 >
+ * ( FR4SkillAnimInfo 사용 시 )
+ * - Skill Anim Key값을 Server로 부터 할당 받을 수 있도록 Replicate 설정
+ * - Skill Anim 키 값에 맞는 Valid Check 조건을 설정 (OnBeginSkillAnim(), IsLockPlaySkillAnim())
+ * ( FR4SkillDetectInfo 사용 시 )
+ * - Detector Key값을 Server로 부터 할당 받을 수 있도록 Replicate 설정
+ * ( Skill 타이밍 체크 )
+ * - Skill 사용을 적절히 체크하여 Skill CoolDownTime을 체크 및 설정 (SetSkillCoolDownTime())
+ */
+UCLASS( Abstract, ClassGroup=(Skill) )
+class RAID4_API UR4AnimSkillBase : public UR4SkillBase
+{
+	GENERATED_BODY()
+
+public:
+	UR4AnimSkillBase();
+
+protected:
+#if WITH_EDITOR
+	virtual void PostEditChangeProperty( FPropertyChangedEvent& PropertyChangedEvent ) override;
+#endif
+
+	virtual void BeginPlay() override;
+	
+protected:
+	// Skill Animation을 Play. 멤버로 등록된 Skill Anim만 Server에서 Play 가능.
+	bool PlaySkillAnim( const FR4SkillAnimInfo& InSkillAnimInfo );
+
+	// Anim을 Play시작 시 호출. Key에 맞춰 할 로직 설정. Server와 Owner Client 에서 호출.
+	virtual void OnBeginSkillAnim( uint32 InSkillAnimKey ) {}
+	
+	// Skill Anim Key 값에 맞는 Skill Anim 을 현재 Play할 수 없는지 확인.
+	// 기본적으로 PlayAnim Server RPC에서 Validation Check에 사용
+	virtual bool IsLockPlaySkillAnim( uint32 InSkillAnimKey ) const { return false; }
+
+private:
+	// Server로 Skill Anim Play를 전송.
+	UFUNCTION( Server, Reliable, WithValidation )
+	void _ServerRPC_PlaySkillAnim( uint32 InSkillAnimKey );
+	
+	// Skill Anim 멤버를 찾아서 DetectNotify <-> FR4SkillDetectEffectInfo를 Bind. 및 Skill Anim 키 부여
+	void _ParseSkillAnimInfo();
+
+	// DetectNotify <-> FR4DetectEffectWrapper 연결
+	void _BindDetectNotifyAndEffect( const TScriptInterface<IR4NotifyDetectInterface>& InDetectNotify, const FR4DetectEffectWrapper& InDetectEffectInfo );
+	
+private:
+	// SkillAnimInfo를 Bind하며 찾은 현재 클래스 멤버에 있는 SkillAnimInfo의 개수, Skill Anim Key 부여용으로도 사용
+	uint32 CachedSkillAnimInfoCount;
+
+	// Key값이 어떤 SkillAnim을 가리키는지 확인하기 위한 CachedMap (Server Only)
+	// { Skill Anim Key, UAnimMontage* }
+	TMap<uint32, TWeakObjectPtr<UAnimMontage>> Server_CachedSkillAnimKey;
+};
