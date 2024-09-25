1cc1bd218fd56302edff8a32e0cee311b8349c73
Imusun <imusun190812@gmail.com>
2024-09-25 12:06:30 +0900

    Feat: Character Movement Component에 Force Move 기능 추가
    - Linear, CurveVector 두가지 방식으로 캐릭터를 강제로 이동 시킬 수 있음
    
    - Linerar
     * 시작지점 -> 목표지점까지 시간에 맞춰 선형보간 이동
    
    - Curve Vector
     * Curve Vector는 Target Loc까지 Duration동안 이동하며, CurveVector의 X,Y,Z를 따라 이동.
     * Curve Vector는 주어진 Duration 동안 각 X,Y,Z의 값의 Curve Y축 값을 추출해 적용하며
     * X,Y,Z값의 Curve Y축 값은 Curve X축(시간당 위치 (0:StartLoc ~ 1:TargetLoc 까지 사용))으로 추출.
     * X,Y,Z값은 시작지점 <-> 목표지점에 선형의 줄을 긋고 시간 비율에 맞는 위치로부터의 Relative Location으로 적용함.
    
    -현재 Local 버전만 만들었음 (Skill 시 Client와 Server에서 동시에 이동할 예정이라), 필요하면 client pkt 관련 추가 구현 필요

--- Source/Raid4/Movement/R4CharacterMovementComponent.cpp
+++ Source/Raid4/Movement/R4CharacterMovementComponent.cpp
@@ -3,19 +3,120 @@
 
 #include "R4CharacterMovementComponent.h"
 
+#include <Curves/CurveVector.h>
+
 #include UE_INLINE_GENERATED_CPP_BY_NAME(R4CharacterMovementComponent)
 
 UR4CharacterMovementComponent::UR4CharacterMovementComponent()
 {
 	PrimaryComponentTick.bCanEverTick = true;
-
-	bWantsInitializeComponent = true;
+	
+	ForceMoveType = ER4ForceMoveType::None;
+	CachedForceMoveStartWorldLoc = FVector::ZeroVector;
+	CachedForceMoveElapsedTime = 0.f;
+	CachedForceMoveDuration = 0.f;
+	CachedForceMoveTargetWorldLoc = FVector::ZeroVector;
+	CachedCurveVector = nullptr;
 }
 
 /**
- *	컴포넌트 초기화.
+ *	Local에서 목표 지점까지 선형으로 Force Movement 설정
+ *	목표 지점까지 Duration동안 선형으로 이동.
+ *	@param InTargetLoc : 목표 지점
+ *	@param InDuration : 걸리는 시간
  */
-void UR4CharacterMovementComponent::InitializeComponent()
+void UR4CharacterMovementComponent::SetForceMovementByLinear_Local( const FVector& InTargetLoc, float InDuration )
 {
-	Super::InitializeComponent();
+	ForceMoveType = ER4ForceMoveType::Linear;
+	_SetupForceMovement( InTargetLoc, InDuration );
 }
+
+/**
+ *	Local에서 CurveVector값을 이용하여 Force Movement 설정
+ *	Target Loc까지 Duration동안 이동하며, CurveVector의 X,Y,Z를 따라 이동.
+ *	Curve Vector는 주어진 Duration 동안 각 X,Y,Z의 값의 Curve Y축 값을 추출해 적용하며
+ *	X,Y,Z값의 Curve Y축 값은 Curve X축(시간당 위치 (0:StartLoc ~ 1:TargetLoc 까지 사용))으로 추출.
+ *	X,Y,Z값은 시작지점 <-> 목표지점에 선형의 줄을 긋고 시간 비율에 맞는 위치로부터의 Relative Location으로 적용함.
+ *	@param InTargetLoc : 목표 지점
+ *	@param InCurveVector : X,Y,Z의 Delta 값을 담은 Curve Vector
+ *	@param InDuration : 걸리는 시간
+ */
+void UR4CharacterMovementComponent::SetForceMovementByCurve_Local( const FVector& InTargetLoc, float InDuration, UCurveVector* InCurveVector )
+{
+	if ( !IsValid( InCurveVector ) )
+	{
+		LOG_WARN( R4Data, TEXT("In Curve Asset is invalid.") );
+		return;
+	}
+
+	ForceMoveType = ER4ForceMoveType::CurveVector;
+	_SetupForceMovement( InTargetLoc, InDuration );
+	
+	CachedCurveVector = InCurveVector;
+}
+
+/**
+ *	Force Movement 준비
+ */
+void UR4CharacterMovementComponent::_SetupForceMovement( const FVector& InTargetLoc, float InDuration )
+{
+	SetMovementMode( MOVE_Custom );
+	CachedForceMoveStartWorldLoc = UpdatedComponent->GetComponentLocation();
+	CachedForceMoveTargetWorldLoc = InTargetLoc;
+	CachedForceMoveElapsedTime = 0.f;
+	CachedForceMoveDuration = InDuration;
+}
+
+/**
+ *	Force Movement 해제
+ */
+void UR4CharacterMovementComponent::_TearDownForceMovement()
+{
+	ForceMoveType = ER4ForceMoveType::None;
+	SetDefaultMovementMode();
+}
+
+/**
+ *	Movement Update
+ */
+void UR4CharacterMovementComponent::OnMovementUpdated( float DeltaSeconds, const FVector& OldLocation, const FVector& OldVelocity )
+{
+	Super::OnMovementUpdated( DeltaSeconds, OldLocation, OldVelocity );
+
+	if ( ForceMoveType == ER4ForceMoveType::None )
+		return;
+	
+	// Force Movement가 설정되어 있다면 진행
+	CachedForceMoveElapsedTime += DeltaSeconds;
+
+	// 현재 시간에 따른 비율
+	float ratio = CachedForceMoveElapsedTime / CachedForceMoveDuration;
+	
+	// 선형 보간한 다음 위치 계산
+	FVector nextLoc = FMath::Lerp( CachedForceMoveStartWorldLoc, CachedForceMoveTargetWorldLoc, ratio );
+	
+	// 목표 지점까지 선형이동 시
+	// Curve vector 이용 시
+	if ( ForceMoveType == ER4ForceMoveType::CurveVector )
+	{
+		if ( !CachedCurveVector.IsValid() )
+		{
+			LOG_WARN( R4Data, TEXT("CachedCurveVector is invalid.") );
+			_TearDownForceMovement();
+			return;
+		}
+
+		// Curve 값을 Relative로 사용
+		nextLoc = nextLoc + CachedCurveVector->GetVectorValue( ratio );
+	}
+	
+	FVector delta = nextLoc - UpdatedComponent->GetComponentLocation();
+	UpdatedComponent->MoveComponent(delta, UpdatedComponent->GetComponentQuat(), true);
+	
+	// 시간이 모두 지났으면 종료
+	if ( CachedForceMoveElapsedTime - CachedForceMoveDuration > -KINDA_SMALL_NUMBER )
+	{
+		_TearDownForceMovement();
+	}
+}
+
--- Source/Raid4/Movement/R4CharacterMovementComponent.h
+++ Source/Raid4/Movement/R4CharacterMovementComponent.h
@@ -6,8 +6,22 @@
 #include "R4CharacterMovementComponent.generated.h"
 
 /**
+ * Force Movement시 사용할 방법.
+ */
+UENUM( BlueprintType )
+enum class ER4ForceMoveType : uint8
+{
+	Linear			UMETA( DisplayName = "목적지까지 선형이동" ),
+	CurveVector		UMETA( DisplayName = "CurveVector 이용" ),
+	None			UMETA( Hidden )
+};
+
+/**
  * Character가 사용할 R4Character Movement Component.
- * TODO : 이걸 써? 말아 ?
+ * Curve Vector는 Target Loc까지 Duration동안 이동하며, CurveVector의 X,Y,Z를 따라 이동.
+ * Curve Vector는 주어진 Duration 동안 각 X,Y,Z의 값의 Curve Y축 값을 추출해 적용하며
+ * X,Y,Z값의 Curve Y축 값은 Curve X축(시간당 위치 (0:StartLoc ~ 1:TargetLoc 까지 사용))으로 추출.
+ * X,Y,Z값은 시작지점 <-> 목표지점에 선형의 줄을 긋고 시간 비율에 맞는 위치로부터의 Relative Location으로 적용함.
  */
 UCLASS()
 class RAID4_API UR4CharacterMovementComponent : public UCharacterMovementComponent
@@ -17,6 +31,42 @@ class RAID4_API UR4CharacterMovementComponent : public UCharacterMovementCompone
 public:
 	UR4CharacterMovementComponent();
 
-	// 컴포넌트를 초기화 한다.
-	virtual void InitializeComponent() override;
+protected:
+	// Movement Update
+	virtual void OnMovementUpdated( float DeltaSeconds, const FVector& OldLocation, const FVector& OldVelocity ) override;
+
+public:
+	// Local에서 목표 지점까지 선형으로 Force Movement 설정
+	void SetForceMovementByLinear_Local( const FVector& InTargetLoc, float InDuration  );
+
+	// Local에서 CurveVector값을 이용하여 Force Movement 설정
+	void SetForceMovementByCurve_Local( const FVector& InTargetLoc, float InDuration, UCurveVector* InCurveVector );
+
+private:
+	// Force Movement 준비
+	void _SetupForceMovement( const FVector& InTargetLoc, float InDuration );
+
+	// Force Movement 해제
+	void _TearDownForceMovement();
+
+private:
+	// Force Move시 사용할 타입
+	UPROPERTY( VisibleInstanceOnly )
+	ER4ForceMoveType ForceMoveType;
+
+	// Force Move를 시작한 지점
+	FVector CachedForceMoveStartWorldLoc;
+	
+	// Force Move 목표 지점
+	FVector CachedForceMoveTargetWorldLoc;
+	
+	// Force Move가 진행된 시간 캐싱
+	float CachedForceMoveElapsedTime;
+
+	// Force Move 총 기간 캐싱
+	float CachedForceMoveDuration;
+	
+	// For Curve Move//
+	// 사용할 Curve Vector.
+	TWeakObjectPtr<UCurveVector> CachedCurveVector;
 };
