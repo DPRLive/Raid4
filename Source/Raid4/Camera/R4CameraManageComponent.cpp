898fd9f932fc62e831cd2cceb95972f533aeafaa
Imusun <imusun190812@gmail.com>
2024-09-26 11:30:34 +0900

    Feat: CameraManageComp에 SpringArm Length 조절기능 추가

--- Source/Raid4/Camera/R4CameraManageComponent.cpp
+++ Source/Raid4/Camera/R4CameraManageComponent.cpp
@@ -2,23 +2,19 @@
 
 
 #include "R4CameraManageComponent.h"
-#include "../Character/R4CharacterBase.h"
+
+#include <Camera/CameraComponent.h>
+#include <GameFramework/SpringArmComponent.h>
 
 #include UE_INLINE_GENERATED_CPP_BY_NAME(R4CameraManageComponent)
 
 UR4CameraManageComponent::UR4CameraManageComponent()
 {
-	PrimaryComponentTick.bCanEverTick = false;
-	bWantsInitializeComponent = true;
-}
+	PrimaryComponentTick.bCanEverTick = true;
 
-/**
- *	컴포넌트 초기화
- */
-void UR4CameraManageComponent::InitializeComponent()
-{
-	Super::InitializeComponent();
-	
+	SpringArmLengthTolerance = 0.5f;
+	CachedNewSpringArmLength = 0.f;
+	CachedSpringArmUpdateSpeed = 0.f;
 }
 
 /**
@@ -27,4 +23,77 @@ void UR4CameraManageComponent::InitializeComponent()
 void UR4CameraManageComponent::BeginPlay()
 {
 	Super::BeginPlay();
+
+	// 필요할 때 켜서 사용
+	SetComponentTickEnabled( false );
+}
+
+/**
+ *  Tick
+ */
+void UR4CameraManageComponent::TickComponent( float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
+{
+	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
+
+	bool bNeedUpdate = false;
+
+	bNeedUpdate |= _UpdateSpringArmLength( DeltaTime );
+
+	if ( !bNeedUpdate )
+	{
+		SetComponentTickEnabled( false );
+	}
+}
+
+void UR4CameraManageComponent::SetCameraComp( UCameraComponent* InCameraComp )
+{
+	CachedCameraComp = InCameraComp;
+}
+
+void UR4CameraManageComponent::SetSpringArmComp( USpringArmComponent* InSpringArComp )
+{
+	CachedSpringArmComp = InSpringArComp;
+}
+
+/**
+ *  Spring Arm Length Update 요청
+ *  @param InNewLength : 변경할 값
+ *  @param InSpeed : 현재 값 -> 변경할 값 까지의 변경 지연 속도, <= 0일 경우 즉시 이동 
+ */
+void UR4CameraManageComponent::SetSpringArmLength( float InNewLength, float InSpeed )
+{
+	if ( !CachedSpringArmComp.IsValid() )
+		return;
+	
+	if ( InSpeed < KINDA_SMALL_NUMBER )
+	{
+		CachedSpringArmComp->TargetArmLength = InNewLength;
+		return;
+	}
+
+	CachedNewSpringArmLength = InNewLength;
+	CachedSpringArmUpdateSpeed = InSpeed;
+	
+	SetComponentTickEnabled( true );
+}
+
+/**
+ *  Spring Arm을 Update
+ *  @return : Update가 되었는지 여부, false 시 더이상 Update가 필요 없다는 뜻
+ */
+bool UR4CameraManageComponent::_UpdateSpringArmLength( float InDeltaTime ) const
+{
+	if ( !CachedSpringArmComp.IsValid() )
+		return false;
+
+	// 차이가 SpringArmLengthTolerance 이하면 중지
+	if ( FMath::Abs( CachedNewSpringArmLength - CachedSpringArmComp->TargetArmLength ) <= SpringArmLengthTolerance )
+		return false;
+	
+	float newLength = FMath::FInterpTo( CachedSpringArmComp->TargetArmLength, CachedNewSpringArmLength, InDeltaTime, CachedSpringArmUpdateSpeed );
+	CachedSpringArmComp->TargetArmLength = newLength;
+
+	LOG_WARN( LogTemp, TEXT("%f, %f"), newLength, CachedNewSpringArmLength );
+	
+	return true;
 }
--- Source/Raid4/Camera/R4CameraManageComponent.h
+++ Source/Raid4/Camera/R4CameraManageComponent.h
@@ -2,23 +2,61 @@
 #pragma once
 
 #include <Components/ActorComponent.h>
+
 #include "R4CameraManageComponent.generated.h"
+
+class UCameraComponent;
+class USpringArmComponent;
 
 /**
  *  Player의 Camera를 관리해주는 컴포넌트
  */
-UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+UCLASS( ClassGroup=(Camera), meta=(BlueprintSpawnableComponent) )
 class RAID4_API UR4CameraManageComponent : public UActorComponent
 {
 	GENERATED_BODY()
 
 public:	
 	UR4CameraManageComponent();
-	
-	// 컴포넌트 초기화
-	virtual void InitializeComponent() override;
-	
+
 protected:
 	virtual void BeginPlay() override;
+
+public:
+	virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction);
+
+public:
+	// Setter
+	void SetCameraComp( UCameraComponent* InCameraComp );
+	void SetSpringArmComp( USpringArmComponent* InSpringArComp );
+
+	// Getter
+	FORCEINLINE UCameraComponent* GetCameraComp( ) const { return CachedCameraComp.Get(); }
+	FORCEINLINE USpringArmComponent* GetSpringArmComp( ) const { return CachedSpringArmComp.Get(); }
+
+	// Spring Arm의 길이를 조정
+	void SetSpringArmLength( float InNewLength, float InSpeed );
 	
+private:
+	// Spring Arm을 Update
+	bool _UpdateSpringArmLength( float InDeltaTime ) const;
+
+private:
+	// 관리중인 Camera Comp
+	UPROPERTY( VisibleInstanceOnly, Transient, Category = "SpringArmUpdate" )
+	TWeakObjectPtr<UCameraComponent> CachedCameraComp;
+
+	// 관리중인 SpringArm Comp
+	UPROPERTY( VisibleInstanceOnly, Transient, Category = "SpringArmUpdate" )
+	TWeakObjectPtr<USpringArmComponent> CachedSpringArmComp;
+
+	// Spring Arm Update 시 허용할 오차값
+	UPROPERTY( EditAnywhere, Category = "SpringArmUpdate", meta = ( UIMin = "0.01", ClampMin = "0.01" ) )
+	float SpringArmLengthTolerance;
+	
+	// Spring Arm Update 시 변해야하는 목표 값
+	float CachedNewSpringArmLength;
+
+	// Spring Arm Update 시 Speed
+	float CachedSpringArmUpdateSpeed;
 };
