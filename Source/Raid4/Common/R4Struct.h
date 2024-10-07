131939d173eef4408f900ed19203b99e3801a428
Imusun <imusun190812@gmail.com>
2024-10-07 14:47:09 +0900

    Feat: AOE를 위한 Asset 추가
    - R4Struct 의 FR4AreaOfEffect 구조체로 감싸서 편하게 사용 가능. ( 실제 Texture에서의 범위 표시 비율을 통한 Size 계산 기능 제공 )
    - AOE를 위한 Neon Ring PNG, M_AreaOfEffect 추가
    
    NeonRing Texture
    <a href="https://www.vecteezy.com/free-png/neon-ring">Vecteezy의 네온 링 PNG</a>

--- Source/Raid4/Common/R4Struct.h
+++ Source/Raid4/Common/R4Struct.h
@@ -10,7 +10,6 @@
 USTRUCT( BlueprintType )
 struct FR4TagValue
 {
-public:
 	GENERATED_BODY()
 
 	FR4TagValue()
@@ -28,4 +27,36 @@ public:
  
 	UPROPERTY( EditDefaultsOnly )
 	float Value;
+};
+
+/**
+ * Common한 구조체 모음
+ */
+
+/**
+ * Area Of Effect. Material과 Texture에서 실제 범위 표시 비율을 설정
+ */
+USTRUCT( BlueprintType )
+struct FR4AreaOfEffect
+{
+	GENERATED_BODY()
+
+	FR4AreaOfEffect()
+	: AOEDecal( nullptr )
+	, RangeRatio( 0.f )
+	{ }
+
+	// Texture의 실제 범위 표시 비율을 고려한, 표시하고 싶은 InActualRadius에 맞춘 Decal의 크기 반환.
+	float GetDecalSizeByActualRadius( float InActualRadius ) const
+	{
+		return InActualRadius / RangeRatio;
+	}
+	
+	// 범위 표시 Decal
+	UPROPERTY( EditDefaultsOnly )
+	TObjectPtr<UMaterialInterface> AOEDecal;
+
+	// Texture에서 중점으로부터 실제 범위 표시에 해당하는 비율.
+	UPROPERTY( EditDefaultsOnly, meta = (ClampMin = 0.f, ClampMax = 1.f, UIMin = 0.f, UIMax = 1.f) )
+	float RangeRatio;
 };
