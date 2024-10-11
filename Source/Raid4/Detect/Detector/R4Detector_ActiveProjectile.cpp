c7b061995a4e42b19f6bb8c2b9b229a2774d1f7d
Imusun <imusun190812@gmail.com>
2024-10-12 02:00:11 +0900

    Feat: Active Detector Projectile 추가
    - 간단한 Detect 투사체

---
+++ Source/Raid4/Detect/Detector/R4Detector_ActiveProjectile.cpp
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "R4Detector_ActiveProjectile.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(R4Detector_ActiveProjectile)
+
+AR4Detector_ActiveProjectile::AR4Detector_ActiveProjectile()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	Direction = FVector( 1.f, 0.f, 0.f );
+	Speed = 0.f;
+}
+
+void AR4Detector_ActiveProjectile::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+
+	FVector deltaLoc = Direction * Speed * DeltaTime;
+	AddActorLocalOffset( deltaLoc, true );
+}
---
+++ Source/Raid4/Detect/Detector/R4Detector_ActiveProjectile.h
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "R4Detector_Active.h"
+#include "R4Detector_ActiveProjectile.generated.h"
+
+/**
+ * 간단히 날아가는 Detector.
+ */
+UCLASS( Abstract, HideCategories = ( Tick, Rendering, Actor, Input, HLOD, Physics, LevelInstance, WorldPartition, DataLayers ))
+class RAID4_API AR4Detector_ActiveProjectile : public AR4Detector_Active
+{
+	GENERATED_BODY()
+
+public:
+	AR4Detector_ActiveProjectile();
+
+public:
+	virtual void Tick( float DeltaTime ) override;
+
+private:
+	// 방향. ( 기존 Actor의 방향의 Relative로 적용 )
+	UPROPERTY( EditAnywhere, Category = "Projectile" )
+	FVector Direction;
+
+	// 속도
+	UPROPERTY( EditAnywhere, Category = "Projectile" )
+	float Speed;
+};
