625f8d8763a266457896be3ab7a3968a940a68cf
Imusun <imusun190812@gmail.com>
2024-10-11 20:15:57 +0900

    Feat: 두개의 Point 사이에서 Origin을 구하는 R4OriginCalculator_PointToPoint 추가
    - 두개의 Calculator를 통해서, A -> B를 바라보는 A 기준 Origin을 구하는 Calculator

---
+++ Source/Raid4/Calculator/Origin/R4OriginCalculator_PointToPoint.cpp
@@ -0,0 +1,50 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "R4OriginCalculator_PointToPoint.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(R4OriginCalculator_PointToPoint)
+
+/**
+ *  두개의 Calculator를 통해서, A -> B를 바라보는 A 기준 Origin을 Calculate
+ *  CDO를 사용!
+ *  @param InRequestObj : 계산을 요청한 Object
+ *  @param InActor : 계산의 기준이 되는 Actor.
+ */
+FTransform UR4OriginCalculator_PointToPoint::CalculateOrigin( const UObject* InRequestObj, const AActor* InActor ) const
+{
+	if ( !IsValid( AOriginCalculator ) || !IsValid(BOriginCalculator) )
+	{
+		LOG_WARN( R4Log, TEXT("UR4OriginCalculator_PointToPoint requires valid A and B Origin Calculator.") );
+		return FTransform::Identity;
+	}
+
+	// A 의 Origin
+	FTransform originA = _CalculateOriginByCDO( AOriginCalculator, InRequestObj, InActor );
+	FTransform originB = _CalculateOriginByCDO( BOriginCalculator, InRequestObj, InActor );
+
+	// A to B Dir Vector
+	FVector dir = originB.GetLocation() - originA.GetLocation();
+	FQuat rot = FRotationMatrix::MakeFromX( dir ).ToQuat();
+
+	// set A origin rotation
+	originA.SetRotation( rot );
+
+	return originA;
+}
+
+/**
+ *  CDO를 사용해서 Calculate
+ */
+FTransform UR4OriginCalculator_PointToPoint::_CalculateOriginByCDO( const TSubclassOf<UObject>& InOriginCalculator, const UObject* InRequestObj, const AActor* InActor ) const
+{
+	const UObject* cdo = InOriginCalculator->GetDefaultObject( true );
+	const IR4OriginCalculatorInterface* originCalculator = Cast<IR4OriginCalculatorInterface>( cdo );
+	if( originCalculator == nullptr )
+	{
+		LOG_WARN( R4Skill, TEXT("Origin Calculator is nullptr.") )
+		return FTransform::Identity;
+	}
+
+	return originCalculator->CalculateOrigin( InRequestObj, InActor );
+}
---
+++ Source/Raid4/Calculator/Origin/R4OriginCalculator_PointToPoint.h
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "R4OriginCalculatorInterface.h"
+
+#include <UObject/Object.h>
+
+#include "R4OriginCalculator_PointToPoint.generated.h"
+
+/**
+ * 두개의 Calculator를 통해서, A -> B를 바라보는 A 기준 Origin을 구하는 Calculator.
+ */
+UCLASS( HideDropdown, NotBlueprintType, Blueprintable, ClassGroup=(Calculator) )
+class RAID4_API UR4OriginCalculator_PointToPoint : public UObject, public IR4OriginCalculatorInterface
+{
+	GENERATED_BODY()
+
+public:
+	// Mesh의 Socket을 기준으로 Origin으로 계산.
+	virtual FTransform CalculateOrigin( const UObject* InRequestObj, const AActor* InActor ) const override;
+
+private:
+	FTransform _CalculateOriginByCDO( const TSubclassOf<UObject>& InOriginCalculator, const UObject* InRequestObj, const AActor* InActor ) const;
+	
+private:
+	// A 지점의 기준이 되는 Origin을 구하는 Calculator
+	UPROPERTY( EditAnywhere, meta = ( MustImplement = "/Script/Raid4.R4OriginCalculatorInterface" ) )
+	TSubclassOf<UObject> AOriginCalculator;
+
+	// B지점의 기준이 되는 Origin을 구하는 Calculator
+	UPROPERTY( EditAnywhere, meta = ( MustImplement = "/Script/Raid4.R4OriginCalculatorInterface" ) )
+	TSubclassOf<UObject> BOriginCalculator;
+};
