c7e81787ac93bd7034af3be7cacb9c82e09e6546
Imusun <imusun190812@gmail.com>
2024-10-16 04:39:41 +0900

    Feat: HUD 기반 추가
    - Corss Hair Asset 추가 (https://www.artofsith.com/dynamic-reticles)
    - Hp Bar 추가

---
+++ Source/Raid4/UI/HUD/R4InGameHUDWidget.cpp
@@ -0,0 +1,43 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "R4InGameHUDWidget.h"
+
+#include "../Status/R4HpBarWidgetInterface.h"
+#include "../Status/R4HpBarWidget.h"
+
+#include <GameFramework/Pawn.h>
+#include <Components/TextBlock.h>
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(R4InGameHUDWidget)
+
+void UR4InGameHUDWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	// HP Bar Setup
+	if( IsValid( HpBarWidget ) )
+	{
+		if ( IR4HpBarWidgetInterface* owner = Cast<IR4HpBarWidgetInterface>( GetOwningPlayerPawn() ) )
+		{
+			HpBarWidget->OnHpBarUpdate.AddWeakLambda( this, [this]()
+			{
+				_OnUpdateHpBar();
+			} );
+		
+			owner->SetupHpBarWidget( HpBarWidget );
+		}
+	}
+}
+
+/**
+ *  HpBar Update시 호출
+ */
+void UR4InGameHUDWidget::_OnUpdateHpBar() const
+{
+	if ( IsValid( HpBarWidget ) && IsValid( TotalHpText ) && IsValid( CurrentHpText ) )
+	{
+		TotalHpText->SetText( FText::FromString( FString::SanitizeFloat( HpBarWidget->GetTotalHp() ) ) );
+		CurrentHpText->SetText( FText::FromString( FString::SanitizeFloat( HpBarWidget->GetCurrentHp() ) ) );
+	}
+}
---
+++ Source/Raid4/UI/HUD/R4InGameHUDWidget.h
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include <Blueprint/UserWidget.h>
+#include "R4InGameHUDWidget.generated.h"
+
+class UTextBlock;
+class UR4HpBarWidget;
+
+/**
+ * InGame에서 사용되는 HUD.
+ */
+UCLASS()
+class RAID4_API UR4InGameHUDWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void NativeConstruct() override;
+
+private:
+	// HpBar Update시 호출
+	void _OnUpdateHpBar() const;
+	
+private:
+	// Hp Bar
+	UPROPERTY( VisibleAnywhere, meta = ( BindWidget ) )
+	TObjectPtr<UR4HpBarWidget> HpBarWidget;
+
+	// TotalHp Text
+	UPROPERTY( VisibleAnywhere, meta = ( BindWidget ) )
+	TObjectPtr<UTextBlock> TotalHpText;
+	
+	// NowHp Text
+	UPROPERTY( VisibleAnywhere, meta = ( BindWidget ) )
+	TObjectPtr<UTextBlock> CurrentHpText;
+};
