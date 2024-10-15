819b87a3905299e6b0c82aa84a04bad2149caca2
Imusun <imusun190812@gmail.com>
2024-10-15 21:58:17 +0900

    Feat: 캐릭터 선택 UI 로직 구성 + UI 추가
    - 캐릭터 초상화보고 선택 가능
    - 캐릭터 Pick 버튼을 누를 시 FIx, Fix되면 더이상 캐릭터 선택이 불가하도록 UI hittest 차단
    - 현재 접속해 있는 Player 확인 및 선택한 캐릭터 확인가능
    - 모든 접속 플레이어가 캐릭터를 선택 완료해야 Server에서 GameStart 가능.
    
    미적 감각은 역시 없는건가..

--- Source/Raid4/UI/CharacterPick/R4CharacterPickerWidget.cpp
+++ Source/Raid4/UI/CharacterPick/R4CharacterPickerWidget.cpp
@@ -2,15 +2,19 @@
 
 
 #include "R4CharacterPickerWidget.h"
-#include "R4CharacterPortraitButton.h"
+#include "../R4ImageIdButton.h"
+#include "../R4ImageTextHorizontalBox.h"
 #include "../../Controller/R4CharacterPickControllerInterface.h"
 #include "../../Data/R4DTDataPushInterface.h"
+#include "../../Data/Character/R4CharacterRow.h"
+#include "../../Data/Character/R4CharacterSrcRow.h"
 #include "../../PlayerState/R4PlayerStateInterface.h"
 #include "../../Game/R4LobbyGameState.h"
 #include "../../Game/R4LobbyGameMode.h"
 
 #include <Components/Button.h>
 #include <Components/HorizontalBox.h>
+#include <Components/VerticalBox.h>
 #include <GameFramework/PlayerState.h>
 
 #include UE_INLINE_GENERATED_CPP_BY_NAME(R4CharacterPickerWidget)
@@ -24,12 +28,12 @@ void UR4CharacterPickerWidget::NativeConstruct()
 	CharacterFixButton->OnClicked.AddDynamic( this, &UR4CharacterPickerWidget::_OnClickCharacterFixButton );
 	GameStartButton->OnClicked.AddDynamic( this, &UR4CharacterPickerWidget::_OnClickGameStartButton );
 
+	// Player State Init
+	_OnChangePlayerStateArray();
+
 	AR4LobbyGameState* gameState = GetWorld() ? GetWorld()->GetGameState<AR4LobbyGameState>() : nullptr;
 	if( IsValid( gameState ) )
 	{
-		for( auto& playerState : gameState->PlayerArray )
-			_OnNewPlayerState( playerState );
-		
 		// Player State의 변동 여부를 수신.
 		gameState->OnChangePlayerArrayDelegate.AddUObject( this, &UR4CharacterPickerWidget::_OnChangePlayerStateArray );
 	}
@@ -42,6 +46,18 @@ void UR4CharacterPickerWidget::NativeConstruct()
 	// UHorizontalBox Setup
 	for ( int32 key = DT_PC_BEGIN; key <= DT_PC_END; key++ )
 		_CreateCharacterPickButton(key);
+}
+
+/**
+ *	Character Pick, 미리보기 실행
+ */
+void UR4CharacterPickerWidget::_OnClickCharacterPortraitButton( int32 InCharacterId )
+{
+	if( IR4DTDataPushInterface* previewPawn = Cast<IR4DTDataPushInterface>( GetOwningPlayerPawn() ) )
+	{
+		previewPawn->PushDTData( InCharacterId );
+		CachedPickedCharacterId = InCharacterId;
+	}
 }
 
 /**
@@ -68,57 +84,199 @@ void UR4CharacterPickerWidget::_OnClickGameStartButton()
 }
 
 /**
- *	 Character가 Fix 되었을 때 호출
- */
-void UR4CharacterPickerWidget::_OnFixCharacterId( int32 InCharacterId )
-{
-	// Character Fix 버튼 비활성화.
-	CharacterFixButton->SetIsEnabled( false );
-	CharacterFixButton->SetVisibility( ESlateVisibility::HitTestInvisible );
-
-	// CharacterBox hit test off.
-	CharacterBox->SetVisibility( ESlateVisibility::HitTestInvisible );
-}
-
-/**
- *	Character Pick, 미리보기 실행
- */
-void UR4CharacterPickerWidget::_OnClickCharacterPick( int32 InCharacterId )
-{
-	if( IR4DTDataPushInterface* previewPawn = Cast<IR4DTDataPushInterface>( GetOwningPlayerPawn() ) )
-	{
-		previewPawn->PushDTData( InCharacterId );
-		CachedPickedCharacterId = InCharacterId;
-	}
-}
-
-/**
  * create & add portrait button
  */
 void UR4CharacterPickerWidget::_CreateCharacterPickButton( int32 InCharacterId )
 {
 	// Add Character Pick btn
-	if ( UR4CharacterPortraitButton* button = NewObject<UR4CharacterPortraitButton>( this, CharacterPortraitButtonClass ) )
+	if ( UR4ImageIdButton* button = NewObject<UR4ImageIdButton>( this, CharacterPortraitButtonClass ) )
 	{
-		button->SetCharacterDTKey( InCharacterId );
-		button->OnPortraitButtonClickedDelegate.AddUObject( this, &UR4CharacterPickerWidget::_OnClickCharacterPick );
-		CharacterBox->AddChild( button );
+		// image
+		auto portrait = _GetCharacterPortrait( InCharacterId );
+		if ( portrait != nullptr )
+			button->SetImage( *_GetCharacterPortrait( InCharacterId ) );
+		
+		button->SetId( InCharacterId );
+		button->OnImageIdButtonClickedDelegate.AddUObject( this, &UR4CharacterPickerWidget::_OnClickCharacterPortraitButton );
+		PortraitButtonBox->AddChild( button );
 	}
 }
 
 /**
- *  On Change Player State.
+ *  On Change Player State. 시원~하게 밀어버리고 처음부터 다시 감시.
  */
 void UR4CharacterPickerWidget::_OnChangePlayerStateArray()
 {
+	AR4LobbyGameState* gameState = GetWorld() ? GetWorld()->GetGameState<AR4LobbyGameState>() : nullptr;
+	if( !IsValid( gameState ) )
+		return;
+
+	// Cached Player State Monitors 모두 제거
+	for( auto& monitorInfo : CachedPlayerStateMonitors )
+	{
+		if( !monitorInfo.PlayerStatePtr.IsValid() )
+			continue;
+		
+		if ( IR4PlayerStateInterface* playerState = Cast<IR4PlayerStateInterface>( monitorInfo.PlayerStatePtr ) )
+		{
+			playerState->OnSetCharacterId().Remove( monitorInfo.CharacterIdHandle );
+			playerState->OnSetPlayerName().Remove( monitorInfo.PlayerNameHandle );
+		}
+	}
+	CachedPlayerStateMonitors.Reset();
+
+	// PlayerPickBox 모두 제거
+	PlayerPickBox->ClearChildren();
+
+	// Portrait Box Btn 모두 활성화.
+	TArray<UWidget*> portraitButtonBoxs = PortraitButtonBox->GetAllChildren();
+	for( const auto& btn : portraitButtonBoxs )
+	{
+		if ( !IsValid( btn ) )
+			continue;
+		
+		btn->SetIsEnabled( true );
+		btn->SetVisibility( ESlateVisibility::Visible );
+	}
+	
+	// 생성
+	for( const auto& playerState : gameState->PlayerArray )
+	{
+		if( IsValid( playerState ) )
+			_AddPlayerStateMonitor( playerState );
+	}
+	
+	// Rebuild
+	RebuildWidget();
 }
 
 /**
- *  On New Player State.
+ *  Player State 감시를 추가 + PlayerPickBox 동기화.
+ *  @param InPlayerState : 감시할 Player State
  */
-void UR4CharacterPickerWidget::_OnNewPlayerState( APlayerState* InPlayerState )
+void UR4CharacterPickerWidget::_AddPlayerStateMonitor( APlayerState* InPlayerState )
 {
-	// Character ID가 Fix 되는지 수신
-	if ( IR4PlayerStateInterface* playerStateInter = Cast<IR4PlayerStateInterface>( InPlayerState ) )
-		playerStateInter->OnSetCharacterId().AddUObject( this, &UR4CharacterPickerWidget::_OnFixCharacterId );
+	if ( !IsValid( InPlayerState ) )
+		return;
+
+	// 새로운 감시 추가
+	if ( IR4PlayerStateInterface* playerState = Cast<IR4PlayerStateInterface>( InPlayerState ) )
+	{
+		// PortraitNameHorizontalBox 생성
+		UR4ImageTextHorizontalBox* widgetBox = NewObject<UR4ImageTextHorizontalBox>( this, PlayerPickBoxElemClass );
+		if( !IsValid( widgetBox ) )
+			return;
+
+		PlayerPickBox->AddChild( widgetBox );
+		
+		// monitor 생성
+		CachedPlayerStateMonitors.Add( FR4PlayerStateMonitorInfo() );
+		int32 monitorIdx = CachedPlayerStateMonitors.Num() - 1;
+
+		CachedPlayerStateMonitors[monitorIdx].PlayerStatePtr = InPlayerState;
+		
+		// 이미 설정된 경우 대비, 감시 시작 전 호출
+		_OnFixCharacterId( monitorIdx, playerState->GetCharacterId() );
+		_OnSetPlayerName( monitorIdx, InPlayerState->GetPlayerName() );
+
+		// Character ID 감시
+		CachedPlayerStateMonitors[monitorIdx].CharacterIdHandle =
+			playerState->OnSetCharacterId().AddWeakLambda( this,
+		[this, monitorIdx]( int32 InId )
+		{
+			_OnFixCharacterId( monitorIdx, InId );
+		} );
+
+		// Player Name 감시
+		CachedPlayerStateMonitors[monitorIdx].PlayerNameHandle =
+			playerState->OnSetPlayerName().AddWeakLambda( this,
+		[this, monitorIdx]( const FString& InName )
+		{
+			_OnSetPlayerName( monitorIdx, InName );
+		} );
+	}
+}
+
+/**
+ *  PlayerState의 Character가 Fix 되었을 때 호출
+ *  @param InMonitorIndex : Player State 감시 목록의 Index
+ */
+void UR4CharacterPickerWidget::_OnFixCharacterId( int32 InMonitorIndex, int32 InCharacterId )
+{
+	if ( !CachedPlayerStateMonitors.IsValidIndex( InMonitorIndex )
+		|| InCharacterId == DTConst::G_InvalidPK )
+		return;
+
+	// Portrait Box Btn 비활성화.
+	TArray<UWidget*> portraitButtonBoxs = PortraitButtonBox->GetAllChildren();
+	for( const auto& btn : portraitButtonBoxs )
+	{
+		if( UR4ImageIdButton* portraitBtn = Cast<UR4ImageIdButton>( btn ) )
+		{
+			if ( portraitBtn->GetId() == InCharacterId )
+			{
+				portraitBtn->SetIsEnabled( false );
+				portraitBtn->SetVisibility( ESlateVisibility::HitTestInvisible );
+			}
+		}
+	}
+	
+	// PlayerPickBox에 Portrait push
+	if( UWidget* pickBox = PlayerPickBox->GetChildAt( InMonitorIndex ) )
+	{
+		if ( UR4ImageTextHorizontalBox* portraitBox = Cast<UR4ImageTextHorizontalBox>( pickBox ) )
+		{
+			auto portrait = _GetCharacterPortrait( InCharacterId );
+			if ( portrait != nullptr )
+				portraitBox->SetImage( *_GetCharacterPortrait( InCharacterId ) );
+		}
+	}
+	
+	// 어 내껀데
+	if( CachedPlayerStateMonitors[InMonitorIndex].PlayerStatePtr == GetOwningPlayerState( ) )
+	{
+		// Character Fix 버튼 비활성화.
+		CharacterFixButton->SetIsEnabled( false );
+		CharacterFixButton->SetVisibility( ESlateVisibility::HitTestInvisible );
+		
+		// PortraitButtonBox hit test off.
+		PortraitButtonBox->SetVisibility( ESlateVisibility::HitTestInvisible );
+	}
+}
+
+/**
+ *  PlayerState의 PlayerName이 Set 되었을 때 호출
+ *  @param InMonitorIndex : Player State 감시 목록의 Index
+ */
+void UR4CharacterPickerWidget::_OnSetPlayerName( int32 InMonitorIndex, const FString& InName )
+{
+	// PlayerPickBox에 Name push
+	if( UWidget* pickBox = PlayerPickBox->GetChildAt( InMonitorIndex ) )
+	{
+		if ( UR4ImageTextHorizontalBox* portraitBox = Cast<UR4ImageTextHorizontalBox>( pickBox ) )
+			portraitBox->SetText( InName );
+	}
+}
+
+/**
+ *  Character DT Key에 맞는 Portrait return
+ */
+const TSoftObjectPtr<UTexture>* UR4CharacterPickerWidget::_GetCharacterPortrait( int32 InCharacterId ) const
+{
+	const FR4CharacterRowPtr characterData( GetWorld(), InCharacterId );
+	if ( !characterData.IsValid() )
+	{
+		LOG_ERROR( R4Data, TEXT("CharacterData is Invalid. PK : [%d]"), InCharacterId );
+		return nullptr;
+	}
+
+	// Get Resource Pk
+	const FR4CharacterSrcRowPtr characterSrcRow( GetWorld(), characterData->ResourceRowPK );
+	if ( !characterData.IsValid() )
+	{
+		LOG_ERROR( R4Data, TEXT("CharacterSrcData is Invalid. PK : [%d]"), InCharacterId );
+		return nullptr;
+	}
+
+	return &characterSrcRow->Portrait;
 }
--- Source/Raid4/UI/CharacterPick/R4CharacterPickerWidget.h
+++ Source/Raid4/UI/CharacterPick/R4CharacterPickerWidget.h
@@ -5,10 +5,36 @@
 #include <Blueprint/UserWidget.h>
 #include "R4CharacterPickerWidget.generated.h"
 
+class UR4ImageTextHorizontalBox;
 class UVerticalBox;
-class UR4CharacterPortraitButton;
+class UR4ImageIdButton;
 class UButton;
 class UHorizontalBox;
+
+/**
+ * PlayerState 감시 정보.
+ */
+USTRUCT( BlueprintType )
+struct FR4PlayerStateMonitorInfo
+{
+	GENERATED_BODY()
+
+	FR4PlayerStateMonitorInfo()
+	: PlayerStatePtr( nullptr )
+	, CharacterIdHandle( FDelegateHandle() )
+	, PlayerNameHandle( FDelegateHandle() )
+	{}
+
+	// 감시중인 Player State Ptr
+	UPROPERTY( Transient, VisibleInstanceOnly )
+	TWeakObjectPtr<APlayerState> PlayerStatePtr;
+
+	// On Set Character id Delegate handle
+	FDelegateHandle CharacterIdHandle;
+	
+	// On Set PlayerName Delegate Handle
+	FDelegateHandle PlayerNameHandle;
+};
 
 /**
  * Character Picker.
@@ -22,6 +48,10 @@ public:
 	virtual void NativeConstruct() override;
 
 private:
+	// Character Pick
+	UFUNCTION()
+	void _OnClickCharacterPortraitButton( int32 InCharacterId );
+	
 	// Character Fix.
 	UFUNCTION()
 	void _OnClickCharacterFixButton();
@@ -29,14 +59,6 @@ private:
 	// Game Start. ( Server )
 	UFUNCTION()
 	void _OnClickGameStartButton();
-
-	// Character가 Fix 되었을 때 호출
-	UFUNCTION()
-	void _OnFixCharacterId( int32 InCharacterId );
-
-	// Character Pick
-	UFUNCTION()
-	void _OnClickCharacterPick( int32 InCharacterId );
 	
 	// create & add portrait button
 	void _CreateCharacterPickButton( int32 InCharacterId );
@@ -44,16 +66,25 @@ private:
 	// On Change Player State.
 	void _OnChangePlayerStateArray();
 
-	// On New Player State.
-	void _OnNewPlayerState( APlayerState* InPlayerState );
+	//  Player State 감시를 추가 + PlayerPickBox 동기화.
+	void _AddPlayerStateMonitor( APlayerState* InPlayerState );
+	
+	// PlayerState의 Character가 Fix 되었을 때 호출
+	void _OnFixCharacterId( int32 InMonitorIndex, int32 InCharacterId );
+
+	// PlayerState의 PlayerName이 Set 되었을 때 호출
+	void _OnSetPlayerName( int32 InMonitorIndex, const FString& InName );
+
+	// Character DT Key에 맞는 Portrait return
+	const TSoftObjectPtr<UTexture>* _GetCharacterPortrait( int32 InCharacterId ) const;
 private:
 	// Character Pick (Button)을 담는 Character List Box.
 	UPROPERTY( VisibleAnywhere, meta = ( BindWidget ) )
-	TObjectPtr<UHorizontalBox> CharacterBox;
+	TObjectPtr<UHorizontalBox> PortraitButtonBox;
 
 	// Player들을 표시하는 Player List Box.
 	UPROPERTY( VisibleAnywhere, meta = ( BindWidget ) )
-	TObjectPtr<UVerticalBox> PlayerBox;
+	TObjectPtr<UVerticalBox> PlayerPickBox;
 	
 	// Character Pick 확정 Button
 	UPROPERTY( VisibleAnywhere, meta = ( BindWidget ) )
@@ -65,8 +96,15 @@ private:
 
 	// Character Picker로 사용할 button
 	UPROPERTY( EditAnywhere )
-	TSubclassOf<UR4CharacterPortraitButton> CharacterPortraitButtonClass;
+	TSubclassOf<UR4ImageIdButton> CharacterPortraitButtonClass;
+
+	// Player의 Character Pick을 보여줄 Element
+	UPROPERTY( EditAnywhere )
+	TSubclassOf<UR4ImageTextHorizontalBox> PlayerPickBoxElemClass;
 	
 	// 현재 선택된 Character ID.
 	int32 CachedPickedCharacterId;
+
+	// 감시 중인 Player State Info Array. Player Pick Box의 Element, Game State의 PlayerArray와 동기화.
+	TArray<FR4PlayerStateMonitorInfo> CachedPlayerStateMonitors;
 };
